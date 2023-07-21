c890d5a3ee82664f63ac18262f56db1fd919a084
gauravpandeyy <89770153+gauravpandeyy@users.noreply.github.com>
2023-07-21 12:59:34 +0530

    Add files via upload

---
+++ MaxSlidingWindow.java
@@ -0,0 +1,29 @@
+//https://leetcode.com/problems/sliding-window-maximum/description/
+/*Example 1:
+
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [3,3,5,5,6,7]
+Explanation:
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+
+ 9 3
+1 2 3 1 4 5 2 3 6
+ */
+
+import java.util.Arrays;
+
+class MaxSlidingWindow {
+    public static void main(String[] args) {
+int[] arr={1,3,-1,-3,5,3,6,7};
+int k=3;
+
+    }
+
+}
