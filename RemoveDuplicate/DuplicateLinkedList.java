947041acf72ad6361fe6d5eaa032d29055eae697
gauravpandeyy <89770153+gauravpandeyy@users.noreply.github.com>
2023-08-13 12:54:26 +0530

    DuplicateLinkedList.java

---
+++ RemoveDuplicate/DuplicateLinkedList.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode node) {
+        if(node==null)
+        {
+            return node;
+        }
+        ListNode head=node;
+        while(node.next!=null)
+        {
+            if(node.val==node.next.val)
+            {
+node.next=node.next.next;
+            }
+            else{
+                node=node.next;
+            }
+        }
+        return head;
+    }
+}
