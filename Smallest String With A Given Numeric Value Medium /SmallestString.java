59e807a3b43a5692bf300ede620025a1fc5091e7
gauravpandeyy <89770153+gauravpandeyy@users.noreply.github.com>
2023-09-01 18:30:27 +0530

    SmallestString.java

---
+++ Smallest String With A Given Numeric Value Medium /SmallestString.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String getSmallestString(int n, int k) {
+        char ans[]=new char[n];
+        int i=0;
+        while(i<n)
+        {
+            ans[i++]='a';
+        }
+        k=k-n;
+       int j=n-1;
+        while(k>0)
+        {
+            if(k<25)
+            {
+                ans[j]=(char)('a'+k);
+                k=0;
+            }
+            else{
+                ans[j]='z';
+                k=k-25;
+            }
+            j--;
+        }
+        return new String(ans);
+    }
+}
