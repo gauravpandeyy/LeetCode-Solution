086b99b44c8746a96144bfa68444a6f078308514
Gaurav Pandey <89770153+gauravpandeyy@users.noreply.github.com>
2023-09-18 11:57:58 +0530

    Time: 1 ms (41.01%), Space: 43.8 MB (89.26%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+ return cyclicsort(nums);
+    }
+     static int cyclicsort(int[] arr)
+  {
+    int i=0;
+    while(i<arr.length)
+    {
+      int crct=arr[i];
+      if( arr[i]<arr.length&&arr[i]!=arr[crct])
+      {
+        swap(arr, i, crct);
+      }
+      else{
+        i++;
+      }
+  }
+  for(int j=0;j<arr.length;j++)
+  {
+      if(arr[j]!=j)
+      {
+          return j;
+      }
+  }
+  return arr.length;
+}
+static void swap(int[] arr,int a , int b)
+{
+int temp=arr[a];
+  arr[a]=arr[b];
+  arr[b]=temp;
+}
+
+}
